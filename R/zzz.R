284cd07bacba1c45b12694c664bd4d96d6b05335
slwu89 <slwu89@berkeley.edu>
2017-05-21 12:39:46 -0700

    clean repo

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("MASH-MBPT (Modular Analysis and Simulation for Human Health - Mosquito-borne Pathogen Transmission): version 0.9")
-}
